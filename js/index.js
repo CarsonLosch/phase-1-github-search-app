9f47c268ddc041f40b9af8655a21df7aa4dc2a27
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-04-16 16:23:22 -0700

    commit tree a5c8d9c874d4e609e28d823b74354fc51a05be28

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
+const formContainer = document.getElementById('github-form')
 
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log("DOM Fully Loaded")
+    const formContainer = document.getElementById('github-form')
+    
+    formContainer.addEventListener('submit', (event) => {
+        event.preventDefault()
+        let inputValue = event.target.search.value
+        getUserInfo(inputValue)
+    })
+    let loadedUsernames = document.querySelectorAll("usernames")
+    console.log(loadedUsernames)
     
 })
+
+function getUserInfo(username) {
+    let userContainer = document.getElementById('user-list')
+    fetch(`https://api.github.com/search/users?q=${username}`)
+    .then(response => response.json())
+    .then(data => {
+        data.items.forEach(user => {
+            userContainer.textContent +=
+            `
+            <li class="usernames">UserName: ${user.login}</li>
+            <li><img src="${user.avatar_url}" class="userpic"></li>
+            <a href="${user.html_url}">GitHub Link</a>
+            `
+        })
+    })
+    .catch(error => {
+        console.log("Something went wrong", error)
+    })
+}
+
+function getUserRepos() {
+    fetch(`https://api.github.com/users/${username}/repos`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+}
